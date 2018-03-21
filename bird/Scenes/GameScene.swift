cde2de88a18585289d86ebbe25dae44a655e51e6
“Alice” <wl02722691@gmail.com>
2018-03-22 00:02:40 +0800

    Angry Birds 5 — Camera Constraints

--- bird/Scenes/GameScene.swift
+++ bird/Scenes/GameScene.swift
@@ -11,26 +11,37 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    let gameCamera = SKCameraNode()//建立遊戲鏡頭
+    var mapNode = SKTileMapNode()
+    
+    let gameCamera = GameCamera()//建立遊戲鏡頭
     var panRecognizer = UIPanGestureRecognizer()
     
     
     override func didMove(to view: SKView) {
-        addCamera()
+        setupLevel()
         setupGestureRecognizers()
         }
     
     func setupGestureRecognizers() {
         guard let view = view else { return }
         panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan))
+        //在觸動UIPanGestureRecognizer時會利用到@objc func pan(sender:UIPanGestureRecognizer)
         view.addGestureRecognizer(panRecognizer)
     }
     
-    func addCamera(){//gmaeCamera設定
+    func setupLevel(){
+        if let mapNode = childNode(withName: "Tile Map Node") as? SKTileMapNode{
+            self.mapNode = mapNode
+        }
+        addCamera()
+    }
+    
+    func addCamera(){
         guard let view = view else {return}
         addChild(gameCamera)
-        gameCamera.position = CGPoint(x: view.bounds.size.width/2, y: view.bounds.size.height/2)
+        gameCamera.position = CGPoint(x: view.bounds.size.width/2, y: view.bounds.size.height/2)//gmaeCamera一開始的設定位置
         camera = gameCamera //resizeFill讀取camera = gameCamera
+        gameCamera.setConstraints(with: self, and: mapNode.frame, to: gameCamera)
         }
     }
 
@@ -39,8 +50,10 @@ extension GameScene{
 
     @objc func pan(sender:UIPanGestureRecognizer) {
         guard let view = view else { return }
-        let translation = sender.translation(in: view)
-        gameCamera.position = CGPoint(x: gameCamera.position.x - translation.x, y: gameCamera.position.y + translation.y)
+        let translation = sender.translation(in: view)//sender回傳手勢移動距離，存在translation
+        gameCamera.position = CGPoint(x: gameCamera.position.x - translation.x,
+                                      y: gameCamera.position.y + translation.y)
+        //手勢移動時，frame的移動計算方式，+-的方式是跟拖曳的方向有關
         sender.setTranslation(CGPoint.zero, in: view)
     }
 }
